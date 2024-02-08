dd4f052e60de264d6d1a5ca9e3370ce6f7fd82a1
gemegoCZ <Vacal.Marek@gmail.com>
2024-02-08 18:40:58 +0100

    1.commit

---
+++ pythonProject/main.py
@@ -0,0 +1,41 @@
+import wave
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io.wavfile import write
+
+obj = wave.open("alastor.wav", "rb")
+
+sample_freq = obj.getframerate()
+n_samples = obj.getnframes()
+signal_wave = obj.readframes(-1)
+
+obj.close()
+
+t_audio = n_samples/sample_freq
+
+print(t_audio)
+print(len(signal_wave))
+print(signal_wave[5:20])
+
+signal_array = np.frombuffer(signal_wave, dtype=np.int16)
+print(np.size(signal_array))
+signal_2 = signal_wave[int(len(signal_wave)/2):]
+signal_array_2 = np.frombuffer(signal_2, dtype=np.int16)
+
+times = np.linspace(0, t_audio, num=len(signal_array))
+
+print(sample_freq)
+
+plt.figure(figsize=(15, 5))
+plt.plot(signal_array)
+plt.figure(figsize=(15, 5))
+plt.plot(times, signal_array)
+plt.title("Audio Signal")
+plt.ylabel("Signal wave")
+plt.xlabel("Time (s)")
+plt.xlim(0, t_audio)
+plt.show()
+
+write("audio.wav", sample_freq, signal_array_2)
+
+
---
+++ pythonProject/test.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot
+
+radius = [1.0, 2.0, 3.0, 4.0]
+area = [3.14159, 12.56636, 28.27431, 50.26544]
+
+matplotlib.pyplot.plot(radius, area)
+matplotlib.pyplot.show()
