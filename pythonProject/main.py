f2aded5fecf63f577b8b4ebededdb8aab344941e
gemegoCZ <Vacal.Marek@gmail.com>
2024-04-21 19:47:02 +0200

    propojení s LED. dáváni na raspberry

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -4,6 +4,8 @@ import struct
 import matplotlib.pyplot as plt
 from scipy.fft import rfft
 from colorsys import hls_to_rgb
+import board
+import neopixel
 
 FORMAT = pa.paInt16
 CHANNELS = 1
@@ -32,6 +34,8 @@ color_choose = []
 color_argmax_2 = 0
 color_argmax_3 = 0
 color_argmax_4 = 0
+pixels = neopixel.NeoPixel(board.D18,144,brightness = 0)
+pixels[0] = []
 # print(f"\033[38;2;0;100;12mHello!\033[0m")
 
 while True:
@@ -48,9 +52,12 @@ while True:
     ax2.set_ylim(0,100000)
     ax2.set_xlim(0,(CHUNK/2))
 
+    x = max(real_time_rfft)
+    pixels.brightness = int(np.interp(x,(0,100000),(0,0.2)))
+
     color_choose.clear()
     for i in range(23):
-        color_choose.append((-(2/(i+3))+1)*max((real_time_rfft[(i*3):((i+1)*7)])))
+        color_choose.append((-(1.9/(i+3))+1)*max((real_time_rfft[(i*3):((i+1)*7)])))
     color_argmax = np.argmax(color_choose)
 
     color_argmax_1 = int(np.interp(color_argmax, (3, 23), (0, 360)))
@@ -60,20 +67,25 @@ while True:
     color_argmax_3 = color_argmax_2
     color_argmax_2 = color_argmax_1
 
-    rgb_list = hls_to_rgb(((color_argmax-25)/360),.5,.8)
+    rgb_list = hls_to_rgb(((color_argmax-27)/360),.5,.8)
     rgb_list = list(rgb_list)
 
     for i in range(3):
         rgb_list[i] = int(np.interp(rgb_list[i], (0, 1), (0, 255)))
     # print(rgb_list)
 
+    pixels[0] = rgb_list
+
+    for i in range(len(pixels),0):
+        pixels[i+1] = pixels[i]
 
 
-    def change_color_text(text, r, g, b):
-        textcolor = f"\033[38;2;{r};{g};{b}m{text}\033[0m"
-        return textcolor
 
-    text = str(chr(9608))
-
-    print(change_color_text(text,rgb_list[0],rgb_list[1],rgb_list[2]), end="")
+    # def change_color_text(text, r, g, b):
+    #     textcolor = f"\033[38;2;{r};{g};{b}m{text}\033[0m"
+    #     return textcolor
+    #
+    # text = str(chr(9608))
+    #
+    # print(change_color_text(text,rgb_list[0],rgb_list[1],rgb_list[2]), end="")
 
